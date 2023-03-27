6a4306b4f347f4241ea7172bb05dd5e7f3e11189
Lucas Correia da Silva <108384166+LucasLCDS@users.noreply.github.com>
2023-03-27 13:37:27 -0300

    Atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,34 +32,15 @@ function loadPokemonItems(offset, limit) {
 
 loadPokemonItems(offset, limit)
 
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-
-    const qtRecordNextPage = offset + limit
-
-if (qtRecordNextPage >= maxRecords) {
-    const newLimit = maxRecords - offset
-    loadPokemonItems(offset, newLimit)
-
-    loadMoreButton.parentElement.removeChild(loadMoreButton)
-} else {
-    loadPokemonItems(offset, limit)
-}
-
-})
+loadMoreButton.removeEventListener('click', loadMoreHandler)
 
 // Adicionar o listener para o evento scroll
 window.addEventListener('scroll', () => {
     // Verificar se o usuário chegou ao final da página
     if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
-        
       offset += limit
-      // Verificar se chegamos ao limite máximo de registros
-      const qtRecordNextPage = offset + limit
-
-      if (qtRecordNextPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItems(offset, newLimit)
+       // Verificar se chegamos ao limite máximo de registros
+      if (offset >= maxRecords) {
         // Remover o botão "load more" se chegarmos ao limite máximo
         loadMoreButton.parentElement.removeChild(loadMoreButton)
       } else {
