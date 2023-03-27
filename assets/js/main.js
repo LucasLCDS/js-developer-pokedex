360d5158398b36a476532017777bc426688da28b
Lucas Correia da Silva <108384166+LucasLCDS@users.noreply.github.com>
2023-03-27 13:10:13 -0300

    Atualizaçãp

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,8 +51,7 @@ if (qtRecordNextPage >= maxRecords) {
 
 window.addEventListener('scroll', () => {
     offset += limit
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        offset += limit
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {S
 
         const qtRecordNextPage = offset + limit
 
